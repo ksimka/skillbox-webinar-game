cee6fa59f20db167579cd9a779ec4c89bb7e4c68
Maksim Kochkin <maksim.kochkin@corp.badoo.com>
2019-09-29 19:35:36 +0300

    Generate random number, save to file.

--- bac.php
+++ bac.php
@@ -5,3 +5,16 @@ require 'lib.php';
 echonl('--== Bulls and Cows ==--');
 echonl('________________________');
 echonl('');
+
+echo 'Generating a random number... ';
+
+$secret = mt_rand(1000, 9999);
+
+echonl('Done!');
+
+$saved = file_put_contents('secret.txt', $secret);
+if ($saved === false) {
+    error('Failed to save to "secret.txt".');
+}
+
+echonl("It's a " . $secret . "!");
--- lib.php
+++ lib.php
@@ -3,3 +3,8 @@
 function echonl(string $s) : void {
     echo $s . PHP_EOL;
 }
+
+function error(string $s) : void {
+    echonl($s);
+    exit(1);
+}
