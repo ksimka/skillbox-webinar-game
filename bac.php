424c14a8f409386cfba79139b37654433e26d60e
Maksim Kochkin <maksim.kochkin@corp.badoo.com>
2019-09-29 20:07:40 +0300

    Game logic, no interactivity so far.

--- bac.php
+++ bac.php
@@ -6,15 +6,53 @@ echonl('--== Bulls and Cows ==--');
 echonl('________________________');
 echonl('');
 
-echo 'Generating a random number... ';
+$secret_filename = 'secret.txt';
 
-$secret = mt_rand(1000, 9999);
+$guess = $argv[1] ?? false;
+if ($guess === false) {
+    echo 'Generating a random number... ';
 
-echonl('Done!');
+    do {
+        $secret = mt_rand(1000, 9999);
+    } while (!is_valid_number($secret, 4));
 
-$saved = file_put_contents('secret.txt', $secret);
-if ($saved === false) {
-    error('Failed to save to "secret.txt".');
+    echonl('Done!');
+
+    $saved = file_put_contents($secret_filename, $secret);
+    if ($saved === false) {
+        error('Failed to save to ' . $secret_filename . '.');
+    }
+} else {
+    // Read the secret from file.
+    if (!file_exists($secret_filename)) {
+        error('File ' . $secret_filename . ' with secret not found! Run game without args.');
+    }
+
+    $secret = file_get_contents($secret_filename);
+
+    // Chech that guess is the same length as secret.
+    if (!is_valid_number($guess, strlen($secret))) {
+        error('Your guess ' . $guess . ' is invalid: it must be legth ' . strlen($secret) . ' and contain unique digits.');
+    }
+
+    $bulls = 0;
+    $cows = 0;
+
+    foreach (str_split($secret) as $si => $secret_char) {
+        foreach (str_split($guess) as $gi => $guess_char) {
+            if ($secret_char === $guess_char) {
+                $bulls++;
+
+                if ($si === $gi) {
+                    $cows++;
+                }
+            }
+        }
+    }
+
+    echonl('Your guess is ' . $guess . ': bulls=' . $bulls . ', cows=' . $cows);
+
+    if ($bulls === strlen($secret) && $cows === strlen($secret)) {
+        echonl('You won! Congratulations!');
+    }
 }
-
-echonl("It's a " . $secret . "!");
--- lib.php
+++ lib.php
@@ -8,3 +8,16 @@ function error(string $s) : void {
     echonl($s);
     exit(1);
 }
+
+function is_valid_number(int $number, int $length) : bool {
+    if (strlen((string)$number) !== $length) {
+        return false;
+    }
+
+    $unique_chars = array_unique(str_split($number));
+    if (count($unique_chars) < $length) {
+        return false;
+    }
+
+    return true;
+}
