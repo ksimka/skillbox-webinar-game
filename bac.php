597718ea8c257b6a97dbbe8482d077e55a32895a
Maksim Kochkin <maksim.kochkin@corp.badoo.com>
2019-10-04 22:58:52 +0300

    add options

--- bac.php
+++ bac.php
@@ -2,6 +2,18 @@
 
 require 'lib.php';
 
+$options = getopt('', ['length:', 'tries:'], $optind); // https://www.php.net/manual/ru/function.getopt.php
+
+$secret_length = $options['length'] ?? 4;
+if ($secret_length < 3 || $secret_length > 6) {
+    echonl(col_red('Invalid length, must be between 3 and 6'));
+    exit(1);
+}
+
+$tries = $options['tries'] ?? 100;
+
+$base = 10 ** ($secret_length - 1);
+
 echonl('--== Bulls and Cows ==--');
 echonl('________________________');
 echonl('');
@@ -9,8 +21,8 @@ echonl('');
 echo 'Generating a random number... ';
 
 do {
-    $secret = mt_rand(1000, 9999);
-} while (!is_valid_number($secret, 4));
+    $secret = mt_rand($base, 10 * $base - 1);
+} while (!is_valid_number($secret, $secret_length));
 
 echonl('Done!');
 
@@ -20,9 +32,14 @@ $try = 1;
 while (true) {
     $guess = readline($try .') ');
 
+    if (empty($guess)) {
+        echonl(col_red('Resigned! The secret was ' . $secret));
+        exit(0);
+    }
+
     // Check that guess is the same length as secret.
-    if (!is_valid_number($guess, strlen($secret))) {
-        echonl('Your guess ' . $guess . ' is invalid: it must be legth ' . strlen($secret) . ' and contain unique digits.');
+    if (!is_valid_number($guess, $secret_length)) {
+        echonl('Your guess ' . $guess . ' is invalid: it must be legth ' . $secret_length . ' and contain unique digits.');
         continue;
     }
 
@@ -48,5 +65,10 @@ while (true) {
     if ($bulls === strlen($secret) && $cows === strlen($secret)) {
         echonl(col_green('You won! Congratulations!'));
         exit(0);
+    }
+
+    if ($try > $tries) {
+        echonl(col_red('That was the last try, you lose! The secret was ' . $secret));
+        exit(1);
     }
 }
